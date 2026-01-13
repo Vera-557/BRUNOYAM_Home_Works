7b9a73abbf17cb78dfa660a0d0c934657756be50
Daiquiri <mestelo@inbox.ru>
2026-01-13 23:42:25 +0300

    Домашние работы

--- src/HomeWork9/Middle/BankAccount/Bank.java
+++ src/HomeWork9/Middle/BankAccount/Bank.java
@@ -3,3 +3,11 @@ package HomeWork9.Middle.BankAccount;
 public class Bank {
     BankAccount [] counts = new BankAccount[10];
  }
+class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(1000);
+        bankAccount.push(1100);
+       // bankAccount.pop(8000);
+        System.out.println(bankAccount.getBalance());
+    }
+}
